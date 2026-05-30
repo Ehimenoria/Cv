42cf42e0cfe74e7e244dbbf3e6d06c1c617636c3
Ehimenoria <sethqq2@gmail.com>
2026-05-30 04:04:48 +0100

    Delete ErrandXpress1 directory

--- ErrandXpress1/login.js
+++
@@ -1,9 +0,0 @@
-// You can add JavaScript for form validation or other interactive features
-document.addEventListener('DOMContentLoaded', function() {
-    const loginButton = document.querySelector('.login-button');
-
-    loginButton.addEventListener('click', function() {
-        //  add your login logic here.
-        alert('Log In button clicked!');
-    });
-});
